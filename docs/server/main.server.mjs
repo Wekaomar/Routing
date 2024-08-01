15948ed1747b7dfca12570301321ffd567e348eb
Wekaomar <elwakilomar350@gmail.com>
2024-08-01 19:06:37 +0300

    first commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WBZT7IQD.mjs";import"./chunk-VGOQNU3K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ContactComponent } from './contact/contact.component';
+import { portfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo:"home",pathMatch:"full" },
+    {path:"home" , component: HomeComponent},
+    {path:"about" , component: AboutComponent},
+    {path:"portfolio" , component: portfolioComponent},
+    {path:"contact" , component: ContactComponent},
+    {path:"**" , component: NotfoundComponent},
+
+];
